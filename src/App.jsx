3e935afa1698ad83f5d94b42555682303e8384da
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-20 11:06:12 +0600

    Added structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Countries from './components/Countries/Countries'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h2>React World Tour</h2>
+      <h1>React World Tour</h1>
+     
+      <Countries></Countries>
     </>
   )
 }
---
+++ src/components/Country.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './Country.css'
+
+const Country = ({country}) => {
+
+   console.log(country);
+   const {name,flags,area,population}=country;
+   const [borderStyles, setBorderStyles] = useState({
+    border: '2px solid gray',
+    borderRadius: '5px',
+    padding: '10px',
+  });
+   const visitHandler=(x)=>{
+    //    alert(x);
+      setBorderStyles({...borderStyles,backgroundColor:'green',color:'white'});
+   }
+  
+    return (
+        <div className='country'>
+
+            <h3>Name:{name.common}</h3>
+            <img src={flags.png} alt="" />
+            <h3>Area: {area} km<sup>2</sup>  </h3>
+            <h3>Population:{population}</h3>
+            <button onClick={()=> visitHandler(3)} style={borderStyles} >Mark As Visited</button>
+            
+        </div>
+    );
+};
+
+export default Country;
