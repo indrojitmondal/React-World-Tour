e831d3a404217d15e5d1caa444ab39dc691f617d
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-20 13:00:21 +0600

    added eventhandler for visitedCountries

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,6 +4,8 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries]=useState([]);
+   
+
     useEffect( ()=>{
 
         fetch('https://restcountries.com/v3.1/all')
@@ -11,14 +13,25 @@ const Countries = () => {
         .then(data=> setCountries(data))
 
     },[])
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const handleVisitedCountries =(country)=>{
+        console.log("add this to your visited country");
+        console.log(country);
+        setVisitedCountries([...visitedCountries,country]);
+    }
+    
     return (
         <div>
               <h3>Number of Countries: {countries.length}</h3>
+              <h3>Visited Countries: {visitedCountries.length} </h3>
                 <div className='countries'>
                    {
                     countries.map( country=>  <Country 
                         key={country.cca3} 
                         country={country}
+                        handleVisitedCountries={handleVisitedCountries}
+                      
+   
                         >
 
                         </Country>)
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,28 +1,39 @@
 import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
 
-   console.log(country);
+const Country = ({country,  handleVisitedCountries}) => {
+
+//    console.log(handleVisitedCountries);
    const {name,flags,area,population}=country;
    const [borderStyles, setBorderStyles] = useState({
     border: '2px solid gray',
     borderRadius: '5px',
     padding: '10px',
   });
-   const visitHandler=(x)=>{
+   const visitHandler=(country)=>{
     //    alert(x);
+    // console.log(country);
       setBorderStyles({...borderStyles,backgroundColor:'green',color:'white'});
    }
   
     return (
-        <div className='country'>
+        <div className='country'>  
 
             <h3>Name:{name.common}</h3>
             <img src={flags.png} alt="" />
             <h3>Area: {area} km<sup>2</sup>  </h3>
             <h3>Population:{population}</h3>
-            <button onClick={()=> visitHandler(3)} style={borderStyles} >Mark As Visited</button>
+            <button
+            onClick={() => {
+             visitHandler({ country });
+             handleVisitedCountries({country});
+
+          
+             }}
+         style={borderStyles} >
+     Mark As Visited
+     </button>
             
         </div>
     );
